747ebf7bf73b1d5141d66ef4020207730b6a282d
Charlize <extrabutterychar@gmail.com>
2024-07-05 10:46:59 -0700

    Setting up components

--- src/Pages/LogInPage.js
+++ src/Pages/LogInPage.js
@@ -1,12 +1,10 @@
 import GrayNavButton from "../Components/GrayNavButton";
 import LoginButton from "../Components/LoginButton";
-import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
-import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import { Grid, Container, Stack } from "@mui/material";
-import Popcorn from '../Assets/Popcorn.png'
 import InputBar from "../Components/InputBar";
 import { Link } from "react-router-dom";
 import SmallTopBar from "../Components/SmallTopBar";
+import PhoneNumber from "../Components/PhoneNumber";
 
 function LoginPage(){
     return(
@@ -18,41 +16,7 @@ function LoginPage(){
                         <InputBar Label={'Enter Username (email)'}></InputBar>
                         <InputBar Label={'Enter password'}></InputBar>
                     </Stack>
-                    <Stack spacing={4}>
-                            <Grid container spacing = {1}>
-                                <Grid item xs = {8}>
-                                    <div className="TextContent">
-                                        Buy Popcorn and Support the Cause!
-                                    </div>
-                                </Grid>
-                                <Grid item xs = {4} textAlign={'center'} alignContent={'center'}>
-                                    <img src={Popcorn} width={'19px'} height={'28px'}></img>
-                                </Grid>
-                            </Grid>
-                            <div className="PhoneNumber">
-                                <Grid container spacing = {1} alignItems={'center'}>
-                                    <Grid item xs={8}>
-                                        <div className="TextContent">
-                                            Want new recommendation automatically sent to your phone?
-                                        </div>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <ThumbUpOffAltIcon style={{color: '#76D9B6'}}></ThumbUpOffAltIcon>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <ThumbDownOffAltIcon></ThumbDownOffAltIcon>
-                                    </Grid>                
-                                </Grid>
-                                <InputBar Label={'Enter Phone Number'}></InputBar>
-                                <div>
-                                    By providing your phone number your agreeing to receive notifications.
-                                </div>              
-                            </div>
-                                
-                    </Stack>
-                    
-
-
+                    <PhoneNumber></PhoneNumber>
                     <Stack spacing={2}>
                         <Link to={'/Recommendations'}><LoginButton></LoginButton></Link>
                         <GrayNavButton Label="Get Recommendations"></GrayNavButton>
--- src/Pages/SignUpPage.js
+++ src/Pages/SignUpPage.js
@@ -2,18 +2,20 @@ import { Container, Stack } from '@mui/material'
 import InputBar from '../Components/InputBar'
 import LoginButton from '../Components/LoginButton'
 import GrayNavButton from '../Components/GrayNavButton'
+import PhoneNumber from '../Components/PhoneNumber'
+import SmallTopBar from '../Components/SmallTopBar'
 function SignUpPage(){
     return(
         <div className="SignUp">
+            <SmallTopBar Name={'Welcome'}></SmallTopBar>
             <Container>
-                <Stack>
+                <Stack spacing={4}>
                         <InputBar Label={'Enter Username(email)'}></InputBar>
                         <InputBar Label={'Enter Stage name'}></InputBar>
                         <InputBar Label={'Enter password'}></InputBar>
                         <InputBar Label={'Re-enter password'}></InputBar>
                 </Stack>
-                
-
+                <PhoneNumber></PhoneNumber>
                 <InputBar Label={'Enter phone number'}></InputBar>
                 <LoginButton></LoginButton>
                 <GrayNavButton Label={'Get Recommendations'}></GrayNavButton>
